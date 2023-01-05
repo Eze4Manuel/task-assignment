34e9b43628c2bcb8719123c79cfb8a59724aa624
eze4manuel <emmanuelezenagu5@gmail.com>
2023-01-05 11:49:13 +0100

    Fix: tested deployment to aws and updated dockerignore files

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,5 @@
 
 module.exports = {
-    server_base_url: process.env.REACT_APP_SERVER_BASE_URL,
+    // server_base_url: 'http://demoalbbackend-282069910.us-east-1.elb.amazonaws.com:5001',
+    server_base_url: 'http://localhost:5001',
 };
